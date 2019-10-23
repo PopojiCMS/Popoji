3c4726e774eda30c4e56e3971008a3409cc22f79
HOSDG\januar.dalapang_gue <djenuar@gmail.com>
2019-10-23 17:04:04 +0800

    Add paste on context menu tinymce

--- po-includes/resources/views/backend/post/create.blade.php
+++ po-includes/resources/views/backend/post/create.blade.php
@@ -54,12 +54,13 @@
 		plugins: [
 			"advlist autolink link image lists charmap print preview hr anchor pagebreak",
 			"searchreplace wordcount visualblocks visualchars insertdatetime media nonbreaking",
-			"table contextmenu directionality emoticons paste textcolor",
+			"table directionality emoticons paste",
 			"save code fullscreen autoresize codesample autosave responsivefilemanager"
 		],
 		menubar : false,
 		toolbar1: "undo redo restoredraft | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist | outdent indent table searchreplace",
 		toolbar2: "| fontsizeselect | styleselect | link unlink anchor | image media emoticons | forecolor backcolor | code codesample fullscreen ",
+		contextmenu: "link paste image imagetools table spellchecker",
 		image_advtab: true,
 		fontsize_formats: "8px 10px 12px 14px 18px 24px 36px",
 		relative_urls: false,
