1678a74f0575fb356baa1fb0138538b81dc55479
HOSDG\januar.dalapang_gue <djenuar@gmail.com>
2019-11-12 10:34:37 +0800

    FIx setting dropdown

--- po-includes/resources/views/backend/settings/form.blade.php
+++ po-includes/resources/views/backend/settings/form.blade.php
@@ -23,13 +23,39 @@
 <div class="form-group">
     {!! Form::label('value', __('setting.value').' *', ['class' => 'control-label']) !!}
 	@if(isset($setting))
-		@if ($setting->options == 'favicon' || $setting->options == 'logo' || $setting->options == 'logo_footer')
+		@if($setting->options == 'favicon' || $setting->options == 'logo' || $setting->options == 'logo_footer')
 			<div class="input-group">
 				{!! Form::text('value', null, ['class' => $errors->has('value') ? 'form-control is-invalid' : 'form-control', 'id' => 'input-filemanager', 'required' => 'required']) !!}
 				<span class="input-group-append">
 					<a href="{{ url('po-content/filemanager/dialog.php?type=1&field_id=input-filemanager&relative_url=1&akey='.env('FM_KEY')) }}" id="filemanager" class="btn btn-secondary"><i class="fa fa-file"></i> {{ __('general.browse') }}</a>
 				</span>
 			</div>
+		@elseif($setting->options == 'maintenance_mode' || $setting->options == 'member_registration' || $setting->options == 'auto_approve_comment')
+			<select class="select-style form-control" id="value" name="value">
+				@if (isset($setting))
+				<option value="{{ $setting->value }}">{{ __('general.selected') }} {{ $setting->value == 'Y' ? 'Yes' : 'No' }}</option>
+				@endif
+				<option value="Y">Yes</option>
+				<option value="N">No</option>
+			</select>
+		@elseif($setting->options == 'mail_protocol')
+			<select class="select-style form-control" id="value" name="value">
+				@if (isset($setting))
+				<option value="{{ $setting->value }}">{{ __('general.selected') }} {{ $setting->value }}</option>
+				@endif
+				<option value="SMTP">SMTP</option>
+				<option value="Mail">Mail</option>
+			</select>
+		@elseif($setting->options == 'sitemap_frequency')
+			<select class="select-style form-control" id="value" name="value">
+				@if (isset($setting))
+				<option value="{{ $setting->value }}">{{ __('general.selected') }} {{ $setting->value }}</option>
+				@endif
+				<option value="daily">daily</option>
+				<option value="weekly">weekly</option>
+				<option value="monthly">monthly</option>
+				<option value="yearly">yearly</option>
+			</select>
 		@else
 			{!! Form::text('value', null, ['class' => $errors->has('value') ? 'form-control is-invalid' : 'form-control', 'required' => 'required']) !!}
 		@endif
