95880e98fb8338b3532ca0a09dc084597d76c9a4
HOSDG\januar.dalapang_gue <djenuar@gmail.com>
2020-10-09 13:50:48 +0800

    Fix og title error

--- po-includes/app/Http/Controllers/GalleryController.php
+++ po-includes/app/Http/Controllers/GalleryController.php
@@ -34,21 +34,21 @@ class GalleryController extends Controller
 			SEOTools::setTitle($album->title.' - '.getSetting('web_name'));
 			SEOTools::setDescription($album->title.' - '.getSetting('web_description'));
 			SEOTools::metatags()->setKeywords(explode(',', getSetting('web_keyword')));
-			SEOTools::setCanonical(getSetting('web_url'));
+			SEOTools::setCanonical(getSetting('web_url') . '/album/' . $album->seotitle);
 			SEOTools::opengraph()->setTitle($album->title.' - '.getSetting('web_name'));
 			SEOTools::opengraph()->setDescription($album->title.' - '.getSetting('web_description'));
-			SEOTools::opengraph()->setUrl(getSetting('web_url'));
+			SEOTools::opengraph()->setUrl(getSetting('web_url') . '/album/' . $album->seotitle);
 			SEOTools::opengraph()->setSiteName(getSetting('web_author'));
 			SEOTools::opengraph()->addImage(asset('po-content/uploads/'.getSetting('logo')));
 			SEOTools::twitter()->setSite('@'.$twitterid[count($twitterid)-1]);
 			SEOTools::twitter()->setTitle($album->title.' - '.getSetting('web_name'));
 			SEOTools::twitter()->setDescription($album->title.' - '.getSetting('web_description'));
-			SEOTools::twitter()->setUrl(getSetting('web_url'));
+			SEOTools::twitter()->setUrl(getSetting('web_url') . '/album/' . $album->seotitle);
 			SEOTools::twitter()->setImage(asset('po-content/uploads/'.getSetting('logo')));
 			SEOTools::jsonLd()->setTitle($album->title.' - '.getSetting('web_name'));
 			SEOTools::jsonLd()->setDescription($album->title.' - '.getSetting('web_description'));
 			SEOTools::jsonLd()->setType('WebPage');
-			SEOTools::jsonLd()->setUrl(getSetting('web_url'));
+			SEOTools::jsonLd()->setUrl(getSetting('web_url') . '/album/' . $album->seotitle);
 			SEOTools::jsonLd()->setImage(asset('po-content/uploads/'.getSetting('logo')));
 			
 			$gallerys = Gallery::leftJoin('users', 'users.id', 'gallerys.created_by')
@@ -65,21 +65,21 @@ class GalleryController extends Controller
 				SEOTools::setTitle('All Category - '.getSetting('web_name'));
 				SEOTools::setDescription('All Category - '.getSetting('web_description'));
 				SEOTools::metatags()->setKeywords(explode(',', getSetting('web_keyword')));
-				SEOTools::setCanonical(getSetting('web_url'));
+				SEOTools::setCanonical(getSetting('web_url') . '/album/all');
 				SEOTools::opengraph()->setTitle('All Category - '.getSetting('web_name'));
 				SEOTools::opengraph()->setDescription('All Category - '.getSetting('web_description'));
-				SEOTools::opengraph()->setUrl(getSetting('web_url'));
+				SEOTools::opengraph()->setUrl(getSetting('web_url') . '/album/all');
 				SEOTools::opengraph()->setSiteName(getSetting('web_author'));
 				SEOTools::opengraph()->addImage(asset('po-content/uploads/'.getSetting('logo')));
 				SEOTools::twitter()->setSite('@'.$twitterid[count($twitterid)-1]);
 				SEOTools::twitter()->setTitle('All Category - '.getSetting('web_name'));
 				SEOTools::twitter()->setDescription('All Category - '.getSetting('web_description'));
-				SEOTools::twitter()->setUrl(getSetting('web_url'));
+				SEOTools::twitter()->setUrl(getSetting('web_url') . '/album/all');
 				SEOTools::twitter()->setImage(asset('po-content/uploads/'.getSetting('logo')));
 				SEOTools::jsonLd()->setTitle('All Category - '.getSetting('web_name'));
 				SEOTools::jsonLd()->setDescription('All Category - '.getSetting('web_description'));
 				SEOTools::jsonLd()->setType('WebPage');
-				SEOTools::jsonLd()->setUrl(getSetting('web_url'));
+				SEOTools::jsonLd()->setUrl(getSetting('web_url') . '/album/all');
 				SEOTools::jsonLd()->setImage(asset('po-content/uploads/'.getSetting('logo')));
 				
 				$gallerys = Album::where('active', '=', 'Y')
--- po-includes/app/Http/Controllers/HomeController.php
+++ po-includes/app/Http/Controllers/HomeController.php
@@ -58,21 +58,21 @@ class HomeController extends Controller
 		SEOTools::setTitle('Not Found - '.getSetting('web_name'));
 		SEOTools::setDescription(getSetting('web_description'));
 		SEOTools::metatags()->setKeywords(explode(',', getSetting('web_keyword')));
-		SEOTools::setCanonical(getSetting('web_url'));
+		SEOTools::setCanonical(getSetting('web_url') . '/404');
 		SEOTools::opengraph()->setTitle('Not Found - '.getSetting('web_name'));
 		SEOTools::opengraph()->setDescription(getSetting('web_description'));
-		SEOTools::opengraph()->setUrl(getSetting('web_url'));
+		SEOTools::opengraph()->setUrl(getSetting('web_url') . '/404');
 		SEOTools::opengraph()->setSiteName(getSetting('web_author'));
 		SEOTools::opengraph()->addImage(asset('po-content/uploads/'.getSetting('logo')));
 		SEOTools::twitter()->setSite('@'.$twitterid[count($twitterid)-1]);
 		SEOTools::twitter()->setTitle('Not Found - '.getSetting('web_name'));
 		SEOTools::twitter()->setDescription(getSetting('web_description'));
-		SEOTools::twitter()->setUrl(getSetting('web_url'));
+		SEOTools::twitter()->setUrl(getSetting('web_url') . '/404');
 		SEOTools::twitter()->setImage(asset('po-content/uploads/'.getSetting('logo')));
 		SEOTools::jsonLd()->setTitle('Not Found - '.getSetting('web_name'));
 		SEOTools::jsonLd()->setDescription(getSetting('web_description'));
 		SEOTools::jsonLd()->setType('WebPage');
-		SEOTools::jsonLd()->setUrl(getSetting('web_url'));
+		SEOTools::jsonLd()->setUrl(getSetting('web_url') . '/404');
 		SEOTools::jsonLd()->setImage(asset('po-content/uploads/'.getSetting('logo')));
 		
 		return response()->view(getTheme('404'), [], 404);
