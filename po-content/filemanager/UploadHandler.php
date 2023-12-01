b8c6d075fff8e81ebec10b23aaf4cffe09707895
Gibran Dimasagung <gibrandev@gmail.com>
2023-12-01 14:05:13 +0700

    Fix: Trying to access array offset on value of type null

--- po-content/filemanager/UploadHandler.php
+++ po-content/filemanager/UploadHandler.php
@@ -493,7 +493,8 @@ class UploadHandler
             $name = $this->upcount_name($name);
         }
         // Keep an existing filename if this is part of a chunked upload:
-        $uploaded_bytes = $this->fix_integer_overflow((int)$content_range[1]);
+        $range = is_null($content_range) ? 0 : $content_range[1];
+        $uploaded_bytes = $this->fix_integer_overflow((int)$range);
         while (is_file($this->get_upload_path($name))) {
             if ($uploaded_bytes === $this->get_file_size(
                     $this->get_upload_path($name))) {
@@ -1426,7 +1427,8 @@ class UploadHandler
         $name = $file_name ? $file_name : $upload['name'][0];
         $res = $this->generate_response($response, $print_response);
         if(is_file($this->get_upload_path($name))){
-            $uploaded_bytes = $this->fix_integer_overflow((int)$content_range[1]);
+            $range = is_null($content_range) ? 0 : $content_range[1];
+            $uploaded_bytes = $this->fix_integer_overflow((int)$range);
             $totalSize = $this->get_file_size($this->get_upload_path($name));
             if ($totalSize - $uploaded_bytes - $this->options['readfile_chunk_size'] < 0) {
                 $this->onUploadEnd($res);
